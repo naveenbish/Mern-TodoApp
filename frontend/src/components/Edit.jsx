c99d8b62d38f64cae4a733f01c22b92ad259ec32
karananeja <karananeja.r@gmail.com>
2024-02-04 18:42:50 +0530

    used recoil root and the hook to fetch the todos

--- frontend/src/components/Edit.jsx
+++ frontend/src/components/Edit.jsx
@@ -1,51 +1,48 @@
-import Popup from "reactjs-popup";
-import EditSvg from "../assets/edit.svg";
-import { useEffect, useState } from "react";
+import Popup from 'reactjs-popup';
+import EditSvg from '../assets/edit.svg';
+import { useState } from 'react';
+import { useGetTodos } from '../hooks/useTodos';
 
 export default function Edit({ todo }) {
   const [title, setTitle] = useState(todo.title);
   const [description, setDescription] = useState(todo.description);
+  const { getTodos } = useGetTodos();
 
-  function UpdateTodos() {
-      fetch("http://localhost:3000/update", {
-        method: "PUT",
-        body: JSON.stringify({
-          id: todo._id,
-          title: title,
-          description: description,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
+  async function UpdateTodos() {
+    await fetch('http://localhost:3000/update', {
+      method: 'PUT',
+      body: JSON.stringify({ id: todo._id, title, description }),
+      headers: { 'Content-Type': 'application/json' },
     });
+    await getTodos();
   }
   return (
     <Popup
       trigger={
         <button>
-          <img src={EditSvg} alt="edit.svg" className="cursor-pointer" />
+          <img src={EditSvg} alt='edit.svg' className='cursor-pointer' />
         </button>
       }
-      position="right center"
+      position='right center'
     >
       {(close) => (
-        <div className="border p-3 rounded-2xl bg-gray-400">
-          <div className="flex justify-between">
+        <div className='border p-3 rounded-2xl bg-gray-400'>
+          <div className='flex justify-between'>
             <input
-              id="title"
+              id='title'
               value={title}
-              className="border-blue-gray-200 text-blue-gray-700 placeholder-shown:border-blue-gray-200 disabled:bg-blue-gray-50 peer w-full h-full border-b bg-transparent pb-1.5 pt-4 font-mono font-bold text-2xl outline outline-0 transition-all focus:border-gray-900 focus:outline-0 disabled:border-0"
+              className='border-blue-gray-200 text-blue-gray-700 placeholder-shown:border-blue-gray-200 disabled:bg-blue-gray-50 peer w-full h-full border-b bg-transparent pb-1.5 pt-4 font-mono font-bold text-2xl outline outline-0 transition-all focus:border-gray-900 focus:outline-0 disabled:border-0'
               onChange={(e) => {
                 const value = e.target.value;
                 setTitle(value);
               }}
             />
           </div>
-          <div className="flex justify-between">
+          <div className='flex justify-between'>
             <input
-              id="description"
+              id='description'
               value={description}
-              className="border-blue-gray-200 text-gray-500 placeholder-shown:border-blue-gray-200 disabled:bg-blue-gray-50 peer w-full h-full border-b bg-transparent pb-1.5 pt-4 font-mono font-bold text-lg outline outline-0 transition-all focus:border-gray-900 focus:outline-0 disabled:border-0"
+              className='border-blue-gray-200 text-gray-500 placeholder-shown:border-blue-gray-200 disabled:bg-blue-gray-50 peer w-full h-full border-b bg-transparent pb-1.5 pt-4 font-mono font-bold text-lg outline outline-0 transition-all focus:border-gray-900 focus:outline-0 disabled:border-0'
               onChange={(e) => {
                 const value = e.target.value;
                 setDescription(value);
@@ -54,9 +51,9 @@ export default function Edit({ todo }) {
           </div>
           <div
             onClick={UpdateTodos}
-            className="flex pt-3 justify-center align-middle"
+            className='flex pt-3 justify-center align-middle'
           >
-            <button onClick={close} className="font-extrabold text-xl">
+            <button onClick={close} className='font-extrabold text-xl'>
               Add
             </button>
           </div>
