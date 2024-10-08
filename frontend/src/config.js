35cd2a8799f25a7513f370f36b2a0408cc550d2e
naveenbish <bishtnitin003@gmail.com>
2024-10-08 23:38:17 +0530

    backend url inserted

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-export const base_url = "http://localhost:3000/api/v1";
+// export const base_url = "http://localhost:3000/api/v1";
+// export const prod_url = "https://mern-todoapp-backend-662y.onrender.com/api/v1";
+export const base_url = "https://mern-todoapp-backend-662y.onrender.com/api/v1";
